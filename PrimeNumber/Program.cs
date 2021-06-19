8b10c2265d367748d4895deeea3187f5910176ed
Soumyaavendata97 <s.mukhopadhyay@avendata-group.com>
2021-06-19 16:21:09 +0530

    Add project files.

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int UserInput = int.Parse(Console.ReadLine());
+            int Divisor = 0;
+            
+            for(int i =1; i <= UserInput; i++)
+            {
+                if (UserInput % i == 0)
+                {
+                    Divisor++;
+                }
+            }
+            if (Divisor == 2)
+            {
+                Console.WriteLine("Prime Number");
+
+            }
+            else
+            {
+                Console.WriteLine("Not a prime number");
+            }
+        }
+    }
+}
